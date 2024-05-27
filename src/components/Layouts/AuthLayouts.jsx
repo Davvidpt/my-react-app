634c4b120f86b822ec5f599275ed8cb8b2506aad
dave <exelandriano@gmail.com>
2024-05-27 16:52:33 +0700

    coba

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -6,7 +6,7 @@ const AuthLayouts = (props) => {
         <div className="w-full shadow-lg border border-gray-100 p-8 rounded-lg max-w-xs">
           <h1 className="text-yellow-500 font-bold text-3xl mb-2">{title}</h1>
           <p className="font-medium text-salte-500 mb-8">
-            Welcome, please enter your details
+            Welcome, please enter your details..
           </p>
           {children}
         </div>
