5b5fcf3e1823e968985d25e71db26c814010cbd9
dave <exelandriano@gmail.com>
2024-05-29 09:39:23 +0700

    aa

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,22 +1,33 @@
 import CardProduct from "../components/Fragments/CardProduct";
+
+
+const products = [
+  {
+    id: 1,
+    title: "Dinasty-90",
+    src: "/images/dinasty-130.jpg",
+  },
+  {
+    id: 2,
+    title: "Dinasty-90",
+    src: "/images/dinasty.jpg",
+  },
+  {
+    id: 3,
+    title: "Dinasty-90",
+    src: "/images/olympus-160.02.jpg",
+  },
+]
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
+      {products.map((product) => (
       <CardProduct>
-        <CardProduct.Header src="/images/dinasty.jpg"></CardProduct.Header>
-        <CardProduct.Body title="Dinasty-90"></CardProduct.Body>
-        <CardProduct.Footer></CardProduct.Footer>
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.Header src="/images/dinasty-130.jpg"></CardProduct.Header>
-        <CardProduct.Body title="Dinasty-130"></CardProduct.Body>
-        <CardProduct.Footer></CardProduct.Footer>
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.Header src="/images/olympus-160.01.jpg"></CardProduct.Header>
-        <CardProduct.Body title="Olympus 160.01"></CardProduct.Body>
-        <CardProduct.Footer></CardProduct.Footer>
-      </CardProduct>
+     <CardProduct.Header src={product.src} />
+     <CardProduct.Body  title={product.title}/>
+     <CardProduct.Footer />
+     </CardProduct>
+      ))}
     </div>
   );
 };
